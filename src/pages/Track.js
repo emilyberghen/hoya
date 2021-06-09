f6c42ca90590c75b74f38b17ac65faec5996c033
Emily <emily.berghen@hotmail.com>
2021-06-09 02:00:31 +0200

    styling predictions

--- src/pages/Track.js
+++ src/pages/Track.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Select from "react-select";
 
+import data from "./../data/trackItems.json";
+import TrackItems from "./../components/track-items/TrackItems";
+
 const TrackPage = () => {
   const options = [
     { value: "all plants", label: "all plants" },
@@ -9,13 +12,25 @@ const TrackPage = () => {
   ];
 
   return (
-    <form>
-      <h3>plant</h3>
+    <form className="track">
+      <h2>plant</h2>
       <div>
-        <input type="radio" value="one" name="one" /> One plant
-        <input type="radio" value="multiple" name="multiple" /> Multiple plants
+        <input type="radio" value="one" name="one" /> one plant
+        <input type="radio" value="multiple" name="multiple" /> multiple plants
       </div>
       <Select options={options} />
+
+      <h2>status</h2>
+      <div className="status">
+        <p className="healthy">healthy</p>
+        <p className="okay">okay</p>
+        <p className="sick">sick</p>
+      </div>
+
+      <h2>track items</h2>
+      <div className="track-items">
+        <TrackItems items={data} />
+      </div>
     </form>
   );
 };
